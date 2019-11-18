bac68326586d9e94c400ead01362801199802e34
João Victor <jooeu40jv@gmail.com>
2019-11-18 13:50:31 -0300

    Update API

--- app/src/main/java/com/example/projeto_nomes/RetrofitInitializer.kt
+++ app/src/main/java/com/example/projeto_nomes/RetrofitInitializer.kt
@@ -1,22 +1,39 @@
 package com.example.projeto_nomes
 
+import com.example.projeto_nomes.services.NomePorLocalidadeService
 import com.example.projeto_nomes.services.NomePorSexoService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInitializer {
 
-    private val retrofit:Retrofit
+    private val retrofitNome:Retrofit
+
+    private val retrofitLocalidade:Retrofit
 
     init {
-        retrofit = Retrofit.Builder()
+        retrofitNome = Retrofit.Builder()
             .baseUrl("https://servicodados.ibge.gov.br/api/v2/censos/nomes/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
     }
 
-    fun nomePorSexoService(): NomePorSexoService {
-        return retrofit.create(NomePorSexoService::class.java)
+    init {
+        retrofitLocalidade = Retrofit.Builder()
+            .baseUrl("http://servicodados.ibge.gov.br/api/v1/localidades/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
     }
+
+    fun nomePorSexoService(): NomePorSexoService {
+        return retrofitNome.create(NomePorSexoService::class.java)
+    }
+
+    fun nomePorLocalidadeService(): NomePorLocalidadeService{
+        return retrofitLocalidade.create(NomePorLocalidadeService::class.java)
+    }
+
+
 }
